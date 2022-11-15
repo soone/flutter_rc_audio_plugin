9546875d87c36559e36b11b6ac6c109a002c6420
Eve <1985245638@qq.com>
2022-11-15 20:10:53 +0800

    修改枚举类型, 修改版本至0.0.3+83

--- android/src/main/kotlin/com/yuanma/rcaudio/flutter_rc_audio_plugin/FlutterRcAudioPlugin.kt
+++ android/src/main/kotlin/com/yuanma/rcaudio/flutter_rc_audio_plugin/FlutterRcAudioPlugin.kt
@@ -63,17 +63,17 @@ class FlutterRcAudioPlugin : FlutterPlugin, MethodCallHandler {
         return RCRTCEngineWrapper.getInstance()
             .setLocalDeviceErrorListener(object : IRCRTCIWLocalDeviceErrorListener {
                 override fun onAudioDeviceError(type: RCRTCIWAudioDeviceErrorType?) {
-                    Log.e("localAudioDeviceError1",type.toString())
-                    // var typeInt:Int = type.ordinal()
+                    // Log.e("localAudioDeviceError1",type.toString())
+                    // var typeInt:Int = type.ordinal
                     // Log.e("audioRouteStatustypeInt",typeInt.toString())
-                    channel.invokeMethod("localAudioDeviceError", type.toString())
+                    channel.invokeMethod("localAudioDeviceError", type.ordinal)
                 }
 
                 override fun onVideoDeviceError(type: RCRTCIWVideoDeviceErrorType?) {
-                    Log.e("localAudioDeviceError2",type.toString())
+                    // Log.e("localAudioDeviceError2",type.toString())
                     // var typeInt:Int = type.ordinal()
                     // Log.e("audioRouteStatustypeInt",typeInt.toString())
-                    channel.invokeMethod("localVideoDeviceError", type.toString())
+                    channel.invokeMethod("localVideoDeviceError", type.ordinal)
                 }
             })
     }
